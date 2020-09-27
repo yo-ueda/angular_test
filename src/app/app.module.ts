35386b413f3cfad8a219aab26a86d16762217932
unknown <yo-ueda@r8.ucom.ne.jp>
2020-09-27 20:39:57 +0900

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './common/navbar/navbar.component';
+import { NavbarComponent } from './common/navbar/nabvar.component';
+
 
 @NgModule({
   declarations: [
--- src/app/common/navbar/nabvar.component.ts
+++ src/app/common/navbar/nabvar.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  templateUrl: './nabvar.component.html',
+  styleUrls: ['./nabvar.component.scss']
 })
 export class NavbarComponent {
 
