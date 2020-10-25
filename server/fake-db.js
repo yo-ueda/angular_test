d596e559d7a3d956c39109730aedeb4bb3df9574
unknown <yo-ueda@r8.ucom.ne.jp>
2020-10-25 10:01:15 +0900

    node.js mongodb

---
+++ server/fake-db.js
@@ -0,0 +1,45 @@
+const Product = require('./model/product')
+class FakeDb{
+  constructor(){
+    this.products = [
+      {
+        name:'カリカリ',
+        weight:'10g',
+        coverImage:'./assets/img/phone-cover.jpg',
+      },
+      {
+        name:'かつおさん',
+        weight:'20g',
+        coverImage:'./assets/img/placeholder.png',
+      },
+      {
+        name:'缶詰',
+        weight:'',
+        coverImage:'./assets/img/phone-cover.jpg',
+      },
+      {
+        name:'ちゅ～る',
+        weight:'5g',
+        coverImage:'./assets/img/phone-cover.jpg',
+      }
+
+    ]
+
+  }
+
+  pushProductsToDb(){
+    this.products.forEach(
+      (product)=>{
+        const newProduct = new Product(product)
+        newProduct.save
+      }
+    )
+  }
+
+  seeDb(){
+    this.pushProductsToDb()
+  }
+}
+
+module.exports = FakeDb
+
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./config/dev')
+const FakeDb = require('./fake-db')
+
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(
+  ()=>{
+    const fakeDb = new FakeDb()
+    fakeDb.seeDb()
+  }
+);
+
+const app = express()
+
+app.get('/products',function(req,res){
+  res.json({'success': true})
+})
+
+const PORT = process.env.PORT || '3001'
+
+app.listen(PORT,function(){
+  console.log('I am runnning')
+})
+
